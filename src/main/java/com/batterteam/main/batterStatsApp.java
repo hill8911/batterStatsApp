cc8459415cccfa009813c0260e732945d93e73ab
hill8911 <hilll8911@clarkstate.edu>
2025-04-29 21:21:31 -0400

    Update batterStatsApp.java
    Added finished layout of the GUI, minus the scene for viewing reports

--- src/main/java/com/batterteam/main/batterStatsApp.java
+++ src/main/java/com/batterteam/main/batterStatsApp.java
@@ -2,7 +2,7 @@
  * Author Name: Batter Team
  * Date: 4/25/25
  * File Name: batterStatsApp.java
- * Last Update: Lillian H. - 4/27/25
+ * Last Update: Lillian H. - 4/29/25
  * Program Description: Main file AND GUI for batterStatsApp.
  */
 
@@ -10,7 +10,7 @@
 ============= CHANGE LOG =============
 Seth I. - 4/25/25 - created main file structure for team to add onto. The below code is an example javaFX code provided by NetBeans
 Lillian H. - 4/27/25 - added basic GUI structure created with Tiffany W
-
+Lillian H. - 4/29/25 - updated formatting of GUI data entry scene
 ======================================
  */
 
@@ -43,7 +43,8 @@ public class batterStatsApp extends Application {
     private Scene menuScene;
     private Scene entryScene;
 
-    private TextField nameField = new TextField();
+    private TextField firstNameField = new TextField();
+    private TextField lastNameField = new TextField();
     private TextField teamField = new TextField();
     private TextField atBatsField = new TextField();
     private TextField hitsField = new TextField();
@@ -54,6 +55,18 @@ public class batterStatsApp extends Application {
     private TextField walksField = new TextField();
     private TextField rbiField = new TextField();
     private DatePicker gameDatePicker = new DatePicker();
+    private TextField oppTeamField = new TextField();
+    private TextField cityField = new TextField();
+    private TextField runsField = new TextField();
+    private TextField baseOnBallsField = new TextField();
+    private TextField sacrificeFliesField = new TextField();
+    private TextField sacrificeBuntsField = new TextField();
+    private TextField hitByPitchField = new TextField();
+    private TextField totalBasesField = new TextField();
+    private TextField leftOnBasesField = new TextField();
+    private TextField stolenBasesField = new TextField();
+    private TextField homePlatesField = new TextField();
+    private ComboBox<String> stateCombo = new ComboBox<>();
     
     @Override
     public void start(Stage primaryStage) {
@@ -83,7 +96,6 @@ public class batterStatsApp extends Application {
         Button viewGameReportBtn = new Button("View Game Report");
         Button viewCumulativeReportBtn = new Button("View Cumulative Report");
         Button helpBtn = new Button("Help");
-        Button resetBtn = new Button("Reset");
         Button exitBtn = new Button("Exit");
 
         // Design
@@ -91,11 +103,10 @@ public class batterStatsApp extends Application {
         viewGameReportBtn.setMinWidth(200);
         viewCumulativeReportBtn.setMinWidth(200);
         helpBtn.setMinWidth(200);
-        resetBtn.setMinWidth(200);
         exitBtn.setMinWidth(200);
 
         // Page layout
-        VBox menuBox = new VBox(15, title, enterStatsBtn, viewGameReportBtn, viewCumulativeReportBtn, helpBtn, resetBtn, exitBtn);
+        VBox menuBox = new VBox(15, title, enterStatsBtn, viewGameReportBtn, viewCumulativeReportBtn, helpBtn, exitBtn);
         menuBox.setAlignment(Pos.CENTER);
         menuBox.setPadding(new Insets(40));
 
@@ -107,8 +118,11 @@ public class batterStatsApp extends Application {
             switchScenes(entryScene);
         });
 
+        // Show an information alert 
+        helpBtn.setOnAction(e -> showHelpAlert());
+        
         // Scene Setup
-        return new Scene(menuBox, 400, 400);
+        return new Scene(menuBox, 400, 300);
     }
     
     // Method to create the data entry scene 
@@ -119,7 +133,7 @@ public class batterStatsApp extends Application {
 
         // Buttons
         Button exitBtn = new Button("Exit");
-        Button resetBtn = new Button("Reset");
+        Button resetBtn = new Button("Reset Stats");
         Button enterBtn = new Button("Enter");
         Button returnBtn = new Button("Return to Menu");
 
@@ -132,35 +146,63 @@ public class batterStatsApp extends Application {
         grid.setPadding(new Insets(5, 5, 5, 5));
         grid.setHgap(10);
         grid.setVgap(10);
-        grid.add(buttonBox, 0, 8, 3, 1);
+        grid.add(buttonBox, 0, 15, 4, 1);
 
         // Labels
-        grid.add(new Label("Batter's Name:"), 0, 1);
-        grid.add(new Label("Batter's Team:"), 0, 2);
+        grid.add(new Label("First Name:"), 0, 1);
+        grid.add(new Label("Last Name:"), 0, 2);
+        grid.add(new Label("Batter's Team:"), 0, 3);
+        grid.add(new Label("Opponent Team:"), 0, 4);
         grid.add(new Label("Date Played:"), 2, 1);
-        grid.add(new Label("At Bats:"), 0, 4);
-        grid.add(new Label("Hits:"), 0, 5);
-        grid.add(new Label("Doubles:"), 0, 6);
-        grid.add(new Label("Triples:"), 0, 7);
-        grid.add(new Label("Home Runs:"), 2, 4);
-        grid.add(new Label("Strike Outs:"), 2, 5);
-        grid.add(new Label("Walks:"), 2, 6);
-        grid.add(new Label("Runs Batted In:"), 2, 7);
-
-        // Text Fields        
-        grid.add(nameField, 1, 1);
-        grid.add(teamField, 1, 2);
-        grid.add(atBatsField, 1, 4);
-        grid.add(hitsField, 1, 5);
-        grid.add(doublesField, 1, 6);
-        grid.add(triplesField, 1, 7);
-        grid.add(homeRunsField, 3, 4);
-        grid.add(strikeOutsField, 3, 5);
-        grid.add(walksField, 3, 6);
-        grid.add(rbiField, 3, 7);
+        grid.add(new Label("Venue City:"), 2, 2);
+        grid.add(new Label("Venue State:"), 2, 3);
+        grid.add(new Label("At Bats:"), 0, 6);
+        grid.add(new Label("Runs:"), 0, 7);
+        grid.add(new Label("Hits"), 0, 8);
+        grid.add(new Label("Runs Batted In:"), 0, 9);
+        grid.add(new Label("Doubles:"), 0, 10);
+        grid.add(new Label("Triples:"), 0, 11);
+        grid.add(new Label("Home Runs:"), 0, 12);
+        grid.add(new Label("Strike Outs:"), 2, 6);
+        grid.add(new Label("Base on Balls:"), 2, 7);
+        grid.add(new Label("Sacrifice Flies:"), 2, 8);
+        grid.add(new Label("Sacrifice Bunts:"), 2, 9);
+        grid.add(new Label("Hit By Pitches:"), 2, 10);
+        grid.add(new Label("Left on Base:"), 2, 11);
+        grid.add(new Label("Total Bases:"), 0, 13);
+        grid.add(new Label("Home Plates:"), 2, 13);
+        grid.add(new Label("Stolen Bases:"), 2, 12);
+  
+        // Format and add Text Fields
+        sizeTextFields();
+        grid.add(firstNameField, 1, 1);
+        grid.add(lastNameField, 1, 2);
+        grid.add(teamField, 1, 3);
+        grid.add(oppTeamField, 1, 4);
+        grid.add(atBatsField, 1, 6);
+        grid.add(runsField, 1, 7);
+        grid.add(hitsField, 1, 8);
+        grid.add(rbiField, 1, 9);
+        grid.add(doublesField, 1, 10);
+        grid.add(triplesField, 1, 11);
+        grid.add(homeRunsField, 1, 12);
+        grid.add(totalBasesField, 1, 13);
+        grid.add(cityField, 3, 2);
+        grid.add(strikeOutsField, 3, 6);
+        grid.add(baseOnBallsField, 3, 7);
+        grid.add(sacrificeFliesField, 3, 8);
+        grid.add(sacrificeBuntsField, 3, 9);
+        grid.add(hitByPitchField, 3, 10);
+        grid.add(leftOnBasesField, 3, 11);
+        grid.add(stolenBasesField, 3, 12);
+        grid.add(homePlatesField, 3, 13);
 
         // Date Picker
         grid.add(gameDatePicker, 3, 1);
+        
+        // Populate Combo Box and Add to Grid
+        populateComboBox();
+        grid.add(stateCombo, 3, 3);
 
         // Exit the app
         exitBtn.setOnAction(e -> stage.close());
@@ -171,7 +213,7 @@ public class batterStatsApp extends Application {
         // Return to menu
         returnBtn.setOnAction(e -> switchScenes(menuScene));
 
-        return new Scene(grid, 550, 275);
+        return new Scene(grid);
     }
     
     // Method to switch scenes
@@ -179,18 +221,73 @@ public class batterStatsApp extends Application {
         stage.setScene(scene);
     }
 
-    // Method to clear all fields 
+    // Method to clear all statistic fields 
     public void clearFields() {
-        nameField.clear();
-        teamField.clear();
         atBatsField.clear();
         hitsField.clear();
+        runsField.clear();
         doublesField.clear();
         triplesField.clear();
         homeRunsField.clear();
         strikeOutsField.clear();
         walksField.clear();
         rbiField.clear();
+        totalBasesField.clear();
+        sacrificeFliesField.clear();
+        sacrificeBuntsField.clear();
+        hitByPitchField.clear();
+        leftOnBasesField.clear();
+        stolenBasesField.clear();
+        homePlatesField.clear();
+        baseOnBallsField.clear();
     }
-    
+
+    // Create and display the alert for the help button on the main menu
+    public void showHelpAlert() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Help");
+        alert.setHeaderText("Help");
+        alert.setContentText("-Enter Batter Stats: Enter information about a batter.\n\n"
+                + "-View Game Report: View statistics about a batter from a specified game.\n\n"
+                + "-View Cumulative Report: View statistics about batters throughout a range of games.");
+        alert.showAndWait();
+    }
+
+    // Populate stateCombo with state abbreviations
+    public void populateComboBox() {
+        String[] states = {"AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", 
+            "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA",
+            "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC",
+            "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT",
+            "VA", "WA", "WV", "WI", "WY"};
+        stateCombo.getItems().addAll(Arrays.asList(states));
+    }
+
+    // Format the text fields
+    public void sizeTextFields() {
+        firstNameField.setMaxSize(100, 100);
+        lastNameField.setMaxSize(100, 100);
+        teamField.setMaxSize(100, 100);
+        oppTeamField.setMaxSize(100, 100);
+        gameDatePicker.setMaxSize(100, 100);
+        cityField.setMaxSize(100, 100);
+        
+        atBatsField.setMaxSize(50, 50);
+        atBatsField.setAlignment(Pos.CENTER_RIGHT);
+        runsField.setMaxSize(50, 50);
+        hitsField.setMaxSize(50, 50);
+        rbiField.setMaxSize(50, 50);
+        doublesField.setMaxSize(50, 50);
+        triplesField.setMaxSize(50, 50);
+        homeRunsField.setMaxSize(50, 50);
+        totalBasesField.setMaxSize(50, 50);
+        strikeOutsField.setMaxSize(50, 50);
+        baseOnBallsField.setMaxSize(50, 50);
+        sacrificeFliesField.setMaxSize(50, 50);
+        sacrificeBuntsField.setMaxSize(50, 50);
+        hitByPitchField.setMaxSize(50, 50);
+        leftOnBasesField.setMaxSize(50, 50);
+        stolenBasesField.setMaxSize(50, 50);
+        homePlatesField.setMaxSize(50, 50);
+    }
 }
